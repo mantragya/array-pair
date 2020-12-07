cc2279b83790cb7a879999be8f1172415afbdb32
mantragya <mantragya.shrivastava@gmail.com>
2020-12-08 00:21:39 +0530

    Try new method

--- src/arraypair.py
+++ src/arraypair.py
@@ -11,11 +11,30 @@ def getPairsCount(arr, n, sum):
 
     return count            
 
+def getPairCount_Test(arr,n,sum):
+    print("Test")
+    if n < 2:
+     return
+    found = set()
+    output = set()
+
+    for num in arr:
+        k = sum - num
+        if k not in found:
+            found.add(num)
+        else:
+            output.add(((min(k,num)),(max(k,sum))))
+    
+    print(output)
+
+    
 
 
 
-arr = [1, 5, 7, -1, 5] 
-n = len(arr) 
-sum = 6
-print(n)
-print("Count of pairs is",getPairsCount(arr, n, sum)) 
+if __name__ == "__main__":    
+    arr = [1, 5, 4,2,7, 5,0,6] 
+    n = len(arr) 
+    sum = 6
+    print(n)
+    getPairCount_Test(arr,n,sum)
+    print("Count of pairs is",getPairsCount(arr, n, sum)) 
